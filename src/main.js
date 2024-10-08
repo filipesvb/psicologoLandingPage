f33418b6c8e34395acdfd410d88016573b6d3ed4
filipesvb <fvb.boas@gmail.com>
2024-10-08 07:21:20 -0300

    changed Header to sticky, fixed dropdown bug (invisible clicking)

--- src/main.js
+++ src/main.js
@@ -5,3 +5,19 @@ const dropdownUl = dropdownMenu.children[0];
 dropdownButton.addEventListener('click', () => {
   dropdownMenu.classList.toggle('active');
 });
+
+document.addEventListener("click", e => {
+
+  if(e.target !== dropdownButton)
+    dropdownMenu.classList.remove('active');
+})
+
+window.addEventListener("scroll", () => {
+
+  if(window.scrollY > 150) {
+    document.querySelector('.pageHeader').classList.add('scrolled');
+  } else {
+    document.querySelector('.pageHeader').classList.remove('scrolled');
+  }
+
+})
