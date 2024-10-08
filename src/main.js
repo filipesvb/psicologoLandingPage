c8da72990e25d53d99e925cda1f75679a3b24096
filipesvb <fvb.boas@gmail.com>
2024-10-08 06:44:51 -0300

    made dropdown look better, changed images to static

--- src/main.js
+++ src/main.js
@@ -3,5 +3,5 @@ const dropdownMenu = document.querySelector('.menu__mobile__list');
 const dropdownUl = dropdownMenu.children[0];
 
 dropdownButton.addEventListener('click', () => {
-  dropdownMenu.classList.toggle('hovered');
+  dropdownMenu.classList.toggle('active');
 });
